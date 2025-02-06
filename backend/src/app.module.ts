b249bae2cfffae7df978943483e62140b9dbed64
Naboussi, Khaoula (Naas) <Khaoula.Naboussi@sgs.com>
2025-02-06 16:25:33 +0100

    set up of front/backend and db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: `postgres`,
+    host: `localhost`,
+    port: 5432,
+    username: `poll_user`,
+    password: `poll_password`,
+    database: `poll_db`,
+    entities: [],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
